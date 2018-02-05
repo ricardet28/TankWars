66725046af775186888c1372274b3ec16d3c175c
Ricardo <lacajadericky@gmail.com>
2018-02-05 02:30:59 +0100

    chapter 5 completed

--- Assets/Scripts/Shell/ShellExplosion.cs
+++ Assets/Scripts/Shell/ShellExplosion.cs
@@ -19,13 +19,59 @@ public class ShellExplosion : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Find all the tanks in an area around the shell and damage them.
+        // Find all the colliders in an area around the shell and damage them.
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+
+        for (int i = 0; i<colliders.Length; i++)
+        {
+            //Take the rigidbodys of these colliders.
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+
+            if (!targetRigidbody)
+                continue;
+            //Add a explosion force to them.
+            targetRigidbody.AddExplosionForce(m_ExplosionForce, this.transform.position, m_ExplosionRadius);
+
+            //Take the targetHealth script
+            TankHealth targetHealth = targetRigidbody.gameObject.GetComponent<TankHealth>();
+
+            if (!targetHealth)
+                continue;
+            //Calculate damage to do.
+            float damage = CalculateDamage(targetRigidbody.position);
+
+            //Apply the damage.
+            targetHealth.TakeDamage(damage);
+
+            //..the same as...
+            //targetRigidbody.gameObject.GetComponent<TankHealth>().TakeDamage(damage);
+
+        }
+
+        m_ExplosionParticles.transform.parent = null;
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        Destroy(m_ExplosionParticles.gameObject, m_ExplosionParticles.main.duration);
+        Destroy(gameObject);
     }
 
 
     private float CalculateDamage(Vector3 targetPosition)
     {
         // Calculate the amount of damage a target should take based on it's position.
-        return 0f;
+        Vector3 explosionToTarget = targetPosition - transform.position;
+
+        float explosionDistance = explosionToTarget.magnitude;
+
+        float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius; //the factor to multiply the damage... between 0 and 1... depending on how big is explosionDistance...
+
+        float damage = relativeDistance * m_MaxDamage;
+
+        damage = Mathf.Max(0f, damage);
+
+        return damage;
+
+
     }
 }
